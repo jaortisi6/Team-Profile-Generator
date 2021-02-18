5fee3d20626b79a4ba5002a5a0d9a6917684d22a
jaortisi6 <jaortisi@gmail.com>
2021-02-17 22:37:39 -0500

    Update Intern.test.js

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,38 +2,34 @@
 // test for name id and email and github
 const { TestResult } = require("@jest/types");
 
-
-const Intern = require('../lib/Intern');
-
+const Intern = require("../lib/Intern");
 
 // test for name, email, id, role validity
 
+const testIntern = new Intern("Joe", "jaortisi@gmail.com", 6, "intern", "MSU");
 
-const testIntern = new Intern("Joe", "jaortisi@gmail.com", 6 , "intern", "MSU")
+test("has a name", () => {
+  expect(testIntern.name).toEqual(expect.any(String));
+  expect(testIntern.name.length).toBeGreaterThan(2);
+});
 
-test('has a name', () => {
-    expect(testIntern.name).toEqual(expect.any(String))
-    expect(testIntern.name.length).toBeGreaterThan(2)
-})
+test("has an email a valid email", () => {
+  expect(testIntern.email).toEqual(expect.stringContaining("@"));
+});
 
-test('has an email a valid email', () =>{
-    expect(testIntern.email).toEqual(expect.stringContaining('@'))
-})
+test("has a role ofintern", () => {
+  expect(testIntern.role).toBe("intern");
+});
 
-test('has a role ofintern', () => {
-    expect(testIntern.role).toBe('intern')
-})
+test("Id has value", () => {
+  expect(testIntern.id).toEqual(expect.any(Number));
+});
 
-test('Id has value', () =>{
-    expect(testIntern.id).toEqual(expect.any(Number))
-})
+test("Entered a school", () => {
+  objectKeys = Object.keys(testIntern);
+  keyGH = objectKeys[4];
 
-test('Entered a school', () =>{
-    objectKeys= Object.keys(testIntern)
-    keyGH = objectKeys[4]
+  expect(keyGH).toBe("school");
 
-    expect(keyGH).toBe('school')
-
-    expect(testIntern.school).toEqual(expect.any(String))
-
-})
+  expect(testIntern.school).toEqual(expect.any(String));
+});
