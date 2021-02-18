9e35649316df48ea7ab57df27a96a4357fd52a77
jaortisi6 <jaortisi@gmail.com>
2021-02-17 22:37:33 -0500

    Update Employee.test.js

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,28 +1,23 @@
 const { TestResult } = require("@jest/types");
 
-
-const Employee = require('../lib/Employee');
-
+const Employee = require("../lib/Employee");
 
 // test for name, email, id, role validity
 
+const testEmployee = new Employee("Joe", "jaortisi@gmail.com", 6, "employee");
 
-const testEmployee = new Employee("Joe", "jaortisi@gmail.com", 6, "employee")
+test("has a name", () => {
+  expect(testEmployee.name).toEqual(expect.any(String));
+  expect(testEmployee.name.length).toBeGreaterThan(2);
+});
+test("has an email a valid email", () => {
+  expect(testEmployee.email).toEqual(expect.stringContaining("@"));
+});
 
-test('has a name', () => {
-    expect(testEmployee.name).toEqual(expect.any(String))
-    expect(testEmployee.name.length).toBeGreaterThan(2)
-    
-    
-})
-test('has an email a valid email', () =>{
-    expect(testEmployee.email).toEqual(expect.stringContaining('@'))
-})
+test("has a role of employee", () => {
+  expect(testEmployee.role).toBe("employee");
+});
 
-test('has a role of employee', () => {
-    expect(testEmployee.role).toBe('employee')
-})
-
-test('Id is a number', () =>{
-    expect(testEmployee.id).toEqual(expect.any(Number))
-})
+test("Id is a number", () => {
+  expect(testEmployee.id).toEqual(expect.any(Number));
+});
